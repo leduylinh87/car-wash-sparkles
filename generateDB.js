173da7124c5c307cc6f9a78640056af6e3dc14e5
duylinh le <leduylinh87@gmail.com>
2018-12-03 00:58:45 +0700

    - Initialize Commit

---
+++ generateDB.js
@@ -0,0 +1,26 @@
+
+var Datastore = require('nedb')
+	, db = new Datastore({ filename: 'CarWash-DB' });
+  
+db.loadDatabase(function (err) {    // Callback is optional
+  // Now commands will be executed
+  
+  console.log(err);
+});
+
+userDB = new Datastore({ filename: 'CarWash-UserDB' });
+
+userDB.loadDatabase(function (err) {    // Callback is optional
+  // Now commands will be executed
+  
+  console.log(err);
+});
+
+var user = { "username": "admin",
+			"password": "123456",
+			"role":"admin"
+           };
+
+userDB.insert(user, function (err, newDoc) {   // Callback is optional
+	console.log(err);
+});
---
+++ main.js
@@ -0,0 +1,59 @@
+const http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var myip = require('quick-local-ip');
+var Datastore = require('nedb')
+var ip = require("ip");
+
+userDB = new Datastore('CarWash-UserDB');
+userDB.loadDatabase(function (err) {
+	if(err != null){
+		console.log(err);
+	}
+});
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+var app = express();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+app.get('/',function(req,res)
+{
+	res.send('Hello World!');
+});
+
+app.post('/', function(req,res)
+{
+    console.log("Receive POST request!");
+	res.send("xxx");
+});
+
+app.post('/Login', function(req,res)
+{	
+	console.log(req.body);
+	userDB.find(req.body, function (err, docs) {
+		if(docs.length > 0){
+			var response = {
+				"status":true,
+				"data":docs[0]
+			};
+			res.send(response);
+		}
+		else {
+			var response = {
+				"status":false
+			};
+			res.send(response);
+		}
+		console.log(docs);
+	});
+});
+
+var server = app.listen(port, function() {
+	var localIp = myip.getLocalIP4();
+	var Ip = ip.address();
+	console.log(`Start server on ${Ip}`);
+});
